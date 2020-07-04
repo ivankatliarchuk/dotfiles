c515dfa1b2b63695d281110c476b1c3e085ed654
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2020-07-04 10:47:03 +0100

    pip incorrect directory

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ git pull
 git submodule sync --recursive
 git submodule update --init --recursive
 
-${dbot} -c ${CONFIG} -d "${BASEDIR}" --plugin-dir vendor/dotbot-brew --plugin-dir vendor/dotbot-pip "${@}"
+${dbot} -c "${BASEDIR}/${CONFIG}" -d "${BASEDIR}" --plugin-dir vendor/dotbot-brew --plugin-dir vendor/dotbot-pip "${@}"
