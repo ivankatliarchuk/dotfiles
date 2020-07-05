c6cbec3ca7294b5eb5f44f99bf2dbce655880f6b
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2020-07-05 14:00:14 +0100

    remap keys

--- zsh/settings/key-remap.zsh
+++ zsh/settings/key-remap.zsh
@@ -9,3 +9,9 @@ bindkey "^[^[[C"  forward-word              # alt + right
 bindkey "^[^[[D"  backward-word             # alt + left
 bindkey "^[^[[A"  _history-complete-older   # alt + up
 bindkey "^[^[[B"  _history-complete-newer   # alt + down
+
+# Define how zsh handles escape sequences produced by terminal emulator
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\eOH" beginning-of-line
+bindkey "\eOF" end-of-line
