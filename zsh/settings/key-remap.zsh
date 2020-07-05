17d6c99c6eb667ac149286b65f9e4dcd5cce8df6
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2020-07-05 22:15:36 +0100

    python path

--- zsh/settings/key-remap.zsh
+++ zsh/settings/key-remap.zsh
@@ -11,7 +11,7 @@ bindkey "^[^[[A"  _history-complete-older   # alt + up
 bindkey "^[^[[B"  _history-complete-newer   # alt + down
 
 # Define how zsh handles escape sequences produced by terminal emulator
-bindkey "\e[1~" beginning-of-line
-bindkey "\e[4~" end-of-line
-bindkey "\eOH" beginning-of-line
-bindkey "\eOF" end-of-line
+# bindkey "\e[1~" beginning-of-line
+# bindkey "\e[4~" end-of-line
+# bindkey "\eOH" beginning-of-line
+# bindkey "\eOF" end-of-line
