3a20b16ee6d1fac50518b45fdf98bc7e8f3fcd56
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2020-07-09 07:41:38 +0100

    zsh functions

--- zsh/functions/functions.zsh
+++ zsh/functions/functions.zsh
@@ -71,3 +71,14 @@ load-nvmrc
     zcompile "$zcompdump"
   fi
 } &!
+
+# Cache completion if nothing changed - faster startup time
+typeset -i updated_at=$(date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)
+if [ $(date +'%j') != $updated_at ]; then
+  compinit -i
+else
+  compinit -C -i
+fi
+
+# Enhanced form of menu completion called `menu selection'
+zmodload -i zsh/complist
