4f56b8d4df8797287f5ae0600b152c0b1a6c6904
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2021-04-12 20:44:31 +0100

    update main install script

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-cd vendor/powerline-fonts
+: "${GVM_NO_UPDATE_PROFILE:=1}"
 
-./install.sh
+[[ -n "${DEBUG:-}" ]] && set -x
 
 exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
-if ! exists gvm; then
-  $SHELL < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-fi
+show_help() {
+cat << EOF
+Usage: $(basename "$0") :do:
+    install powerline fonts
+    install gvm (Go Version Manager)
+    install helm charts
+EOF
+}
+
+install_fonts() {
+  pushd vendor/powerline-fonts > /dev/null
+    echo "here 1: ${PWD}"
+    ./install.sh
+  popd
+}
+
+install_gvm() {
+  if ! exists gvm; then
+    $SHELL < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) || echo "installed"
+  fi
+}
+
+install_helm() {
+  scripts/helm.add.repo
+}
+
+show_help
+install_fonts
+install_gvm
+install_helm
