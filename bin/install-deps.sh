8a720ec15066e2571a32220bb72a236213db59b2
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2021-04-18 13:07:24 +0100

    refactore install dependencies script

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -7,6 +7,11 @@ set -e
 : "${GVM_NO_UPDATE_PROFILE:=1}"
 
 [[ -n "${DEBUG:-}" ]] && set -x
+# shellcheck source=/dev/null
+[[ -f "scripts/utils" ]] && source scripts/utils
+# @todo: renovate should support that
+# renovate: datasource=github-releases depName=nvm-sh/nvm versioning=loose
+NVM_VERSION=v0.36.0
 
 exists() {
   command -v "$1" >/dev/null 2>&1
@@ -24,25 +29,35 @@ EOF
 }
 
 install_fonts() {
+  action "$1"
   pushd vendor/powerline-fonts > /dev/null
-    echo "here 1: ${PWD}"
     ./install.sh
   popd
+  ok "$1"
 }
 
 install_gvm() {
-  # rm -rf ~/.gvm
+  action "$1"
+  local noerr=true
+  [[ -d "${HOME}/.gvm" ]] && echo "exist"
   if ! exists gvm; then
-    $SHELL < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) || echo "installed"
+    [[ -d "${HOME}/.gvm" ]] && rm -rf "${HOME}/.gvm"
+    if ! $SHELL < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer); then
+      error "failed '$1' aborting..."
+      noerr=false
+    fi
     # gvm install go1.16
     # gvm use
+  fi
+  if [[ -z "${noerr}" ]]; then
+    ok "$1"
   fi
 }
 
 install_node() {
   if ! exists nvm; then
     # installed with brwe
-    # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
     # install default from ~/.nvmrc
     echo "install nvm with brew"
   else
@@ -66,12 +81,41 @@ install_rvm() {
 }
 
 install_helm() {
+  action "$1"
   scripts/helm.add.repo
+  ok "$1"
 }
 
-show_help
-install_fonts
-install_gvm
-install_node
+# show_help
+# install_node
 # install_rvm
-install_helm
+
+install_local() {
+local command=$1
+local arg=$2
+bot "${arg}"
+while true; do
+    response=''
+    read_input "${arg}? [y|N] " response
+    case $response in
+        [Yyes]* )
+          $command "$arg"
+          break;;
+        [Nn]* )
+          warn "skipped ${arg}"
+          break;
+          ;;
+        * ) warn "$no_input";;
+    esac
+done
+}
+
+no_input='Please answer yes or no.'
+msg='install/update and reset fonts cache'
+install_local "install_fonts" "$msg"
+
+msg='install gvm'
+install_local "install_gvm" "$msg"
+
+msg='install & update helm repositories'
+install_local "install_helm" "$msg"
