57715232935b125e387e69346144402e1f608b9e
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2026-02-07 13:23:01 +0000

    update config to work with autosuggestions
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- zsh/settings/bootstrap.zsh
+++ zsh/settings/bootstrap.zsh
@@ -69,7 +69,7 @@ fi
 # fi
 
 # https://github.com/moovweb/gvm/issues/463
-unalias cd
+# unalias cd
 [[ -s "${HOME}/.gvm/scripts/gvm" ]] && source "${HOME}/.gvm/scripts/gvm"
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
